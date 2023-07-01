76bf9236a123a39222b7284cbc61c08f4cd1f0a4
Edward Trinh <edwardt81@hotmail.com>
2023-06-30 18:07:26 -0700

    another one

--- website/index.js
+++ website/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://tj0iagl0od.execute-api.us-east-1.amazonaws.com/Prod/put");
+    let response = await fetch("https://c59b86shle.execute-api.us-east-1.amazonaws.com/Prod/put");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
