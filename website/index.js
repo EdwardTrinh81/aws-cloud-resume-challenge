b6e502aea973669d13ed1bacf1af18a12c8a6bc5
Edward Trinh <edwardt81@hotmail.com>
2023-07-01 15:33:06 -0700

    changed API URL

--- website/index.js
+++ website/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://c59b86shle.execute-api.us-east-1.amazonaws.com/Prod/put");
+    let response = await fetch("https://x9e07j8feb.execute-api.us-east-1.amazonaws.com/Prod/put");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
